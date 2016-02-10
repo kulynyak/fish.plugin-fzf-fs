03bb65d0a5bfdd01f2dc53923979d6a8da4da750
Andriy Kulynyak <kulynyak@gmail.com>
2016-02-10 08:51:26 +0200

    renamed files according to fisherman compatibility chart

--- config.fish
+++ config.fish
@@ -1,13 +1,6 @@
-function init --on-event init_fzf-fs
-  # Detect fzf
-  if not available fzf
+if not available fzf
     echo "🍒 Please install 'fzf' first!"
     echo "brew install fzf"
-    return
-  end
 end
 . /usr/local/opt/fzf/shell/key-bindings.fish
 set -x FZF_DEFAULT_OPTS '-x --bind=ctrl-p:up,ctrl-n:down,;:abort,btab:next-history,tab:previous-history'
-if status --is-interactive
-    emit init_fzf-fs $fisher_config/conf.d
-end
